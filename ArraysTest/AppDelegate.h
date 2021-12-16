01a1a3cde2dea02ea5337a01a0037ff6681ad1e4
dmitriyshirvanyan <dad.boss353@gmail.com>
2021-12-16 13:33:56 +0300

    ArraysTest

---
+++ ArraysTest/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  ArraysTest
+//
+//  Created by dmitriyshirvanyan on 15.12.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ArraysTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ArraysTest
+//
+//  Created by dmitriyshirvanyan on 15.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
